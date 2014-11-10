81d574696ef2ad8552332e7313ffe0d9fb848dc6
Ptolemy <ptolemybarnes@gmail.com>
2014-11-10 16:30:45 -0500

    deleted test-first-ruby folder as rspec was not working in it.

--- test-first-ruby/lib/01_temperature.rb
+++
@@ -1,7 +0,0 @@
-def ftoc(temp)
-((temp-32)*0.5555555556).to_i
-end
-
-def ctof(temp)
-((temp*9.0)/5.0)+32
-end
--- test-first-ruby/lib/06_performance_monitor.rb
+++
@@ -1,7 +0,0 @@
-def measure(n=1)
-start_time = Time.now
-n.times do
-	yield
-end
-(Time.now - start_time) / n
-end
