a36a022aa30c1e96f8c8d86a094fe4b629e0dd7d
Ptolemy <ptolemybarnes@gmail.com>
2014-12-22 20:45:48 +0000

    Got collapse (my version of inject) down to 7 lines.

--- LAZER!/lib/map_with_index.rb
+++ LAZER!/lib/map_with_index.rb
@@ -24,10 +24,7 @@ class Array
     start = (args[0].is_a? Numeric ) ? args[0] : nil
     return collapse(start, &args[-1].to_proc) unless block_given?
     pile, idx = (start ? start : self[0]), (start ? 0 : 1)
-    while self[idx]
-      pile = yield(pile, self[idx])
-      idx += 1
-    end
+    self.each_with_index {|item, idx| pile = yield(pile, item) if start || idx != 0}
     pile
   end
 
