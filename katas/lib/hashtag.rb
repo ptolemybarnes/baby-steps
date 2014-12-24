49c9c1686f0f57b49fc1de43031d12b2980fa9d9
Ptolemy <ptolemybarnes@gmail.com>
2014-12-24 13:21:00 +0000

    Refactor solution to hashtag kata.

--- katas/lib/hashtag.rb
+++ katas/lib/hashtag.rb
@@ -2,12 +2,27 @@
 # Write a method that captures words prefixed by hashtags.
 
 def get_hashtags(string)
-  string = string.prepend(" ")
+  string.prepend(" ")
   string = string.split(/ #+/)
-  string = string.select {|string| string[0] != " " }
-  string = string.reject {|string| string.include?('#')}
-  string = string.map {|string| string.include?(' ') ? string[0..string.index(" ")] : string}
-  string = string.map {|string| string.rstrip }
-  string = string.reject {|string| string == "" }
-  string = string.select {|string| string[0].match(/[a-zA-Z]/)}
+  string.reject! {|string| string[0] == " " }
+  string.reject! {|string| string.include?('#')}
+  string.map!    {|string| string.include?(' ') ? string[0..string.index(" ")] : string}
+  string.map!    {|string| string.rstrip }
+  string.reject! {|string| string == "" }
+  string.select  {|string| string[0].match(/[a-zA-Z]/)}
 end
+
+# reflections: I began by trying to find a regex that would match exactly what I want.
+# this approach proved time consuming and the right expression ultimately elusive.
+# I am not happy with the above code as it is very hard to read.
+
+# solution from CodeWars:
+
+=begin
+
+def get_hashtags(post)
+  post.split.grep(/^#+(\w+)$/) { $1 }
+end
+
+  
+=end
