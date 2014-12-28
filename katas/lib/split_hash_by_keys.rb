cd66c6c5a7bb73de139569cec6caa242f6b426ac
Ptolemy <ptolemybarnes@gmail.com>
2014-12-28 17:24:28 +0000

    Refactor split_hash_by_keys method.

--- katas/lib/split_hash_by_keys.rb
+++ katas/lib/split_hash_by_keys.rb
@@ -1,4 +1,5 @@
 =begin 
+# From CodeWars: http://www.codewars.com/kata/52d0f9bc48155f574c0001b7/train/ruby
 Write a method which takes arguments:
 
 hash
@@ -18,8 +19,7 @@ split_hash_by_key( { :a=>1, :b=>2, :c=>3, :d=>4, :e=>5, :f=>6 }, :c, :e )
 
 def split_hash_by_key hash, *splitters
   raise 'One of the key given as argument is incorrect!' unless all_splitters_present? hash, splitters
-  slices = hash.slice_before {|key,value| splitters.include?(key) }.to_a
-  slices.map {|slice| Hash[slice]}
+  hash.slice_before {|key,v| splitters.include?(key) }.map {|slice| Hash[slice]}
 end
 
 def all_splitters_present? hash, splitters
--- katas/spec/split_hash_by_keys_spec.rb
+++ katas/spec/split_hash_by_keys_spec.rb
@@ -8,7 +8,7 @@ describe 'split' do
     expect(split_hash_by_key( { :a => 1, :b => 2},                               :a )).to eq([ {:a => 1, :b => 2} ])
   end
 
-  context 'when passed a splitter not present as a key in the hash' do
+  context 'when passed a splitter that is not a hash key' do
     it 'raises an error' do
       expect( lambda {split_hash_by_key( { :a=>1, :b=>2, :c=>3, :d=>4, :e=>5, :f=>6 }, 'b' )}).to raise_error('One of the key given as argument is incorrect!')
     end
