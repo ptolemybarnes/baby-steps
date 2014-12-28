0d1989dc8938511a302d9dfbcb14de258ade7e53
Ptolemy <ptolemybarnes@gmail.com>
2014-12-28 17:17:55 +0000

    Solve split_hash_by_keys kata.

---
+++ katas/lib/split_hash_by_keys.rb
@@ -0,0 +1,27 @@
+=begin 
+Write a method which takes arguments:
+
+hash
+keys (comma-separated symbols or strings)
+and it splits hash by given keys and returns array of hashes.
+
+If a key given as an argument is not present in the hash,
+the method should raise an exception
+
+In case there are any empty hash after splitting,
+it should be removed from resultant array.
+
+For example:
+# returns [ {:a=>1, :b=>2}, {:c=>3, :d=>4}, {:e=>5, :f=>6} ]
+split_hash_by_key( { :a=>1, :b=>2, :c=>3, :d=>4, :e=>5, :f=>6 }, :c, :e )
+=end
+
+def split_hash_by_key hash, *splitters
+  raise 'One of the key given as argument is incorrect!' unless all_splitters_present? hash, splitters
+  slices = hash.slice_before {|key,value| splitters.include?(key) }.to_a
+  slices.map {|slice| Hash[slice]}
+end
+
+def all_splitters_present? hash, splitters
+  splitters.all? {|splitter| hash.keys.include?(splitter) }
+end
---
+++ katas/spec/split_hash_by_keys_spec.rb
@@ -0,0 +1,16 @@
+require './lib/split_hash_by_keys'
+
+describe 'split' do
+  it 'returns an array of hashes split by the given keys' do
+    expect(split_hash_by_key( { :a=>1, :b=>2, :c=>3, :d=>4, :e=>5, :f=>6 }, :c, :e  )).to eq([ {:a=>1, :b=>2}, {:c=>3, :d=>4}, {:e=>5, :f=>6} ])
+    expect(split_hash_by_key( { :a=>1, :b=>2, :c=>3, :d=>4, :e=>5, :f=>6 }, :b, :f  )).to eq([ {:a=>1}, {:b=>2, :c=>3, :d=>4, :e=>5}, {:f=>6} ])
+    expect(split_hash_by_key( { 'a'=>1, 'b'=>2, 'c'=>3, 'd'=>4, 'e'=>5, 'f'=>6 },'d')).to eq([ {"a"=>1, "b"=>2, "c"=>3}, {"d"=>4, "e"=>5, "f"=>6} ])
+    expect(split_hash_by_key( { :a => 1, :b => 2},                               :a )).to eq([ {:a => 1, :b => 2} ])
+  end
+
+  context 'when passed a splitter not present as a key in the hash' do
+    it 'raises an error' do
+      expect( lambda {split_hash_by_key( { :a=>1, :b=>2, :c=>3, :d=>4, :e=>5, :f=>6 }, 'b' )}).to raise_error('One of the key given as argument is incorrect!')
+    end
+  end
+end
